765f7e62c350ea8ddb4dfb9cdae89dfcf68bd0d5
Harish Bugata <harish.bugata@hotmail.com>
2019-09-15 12:56:55 +0530

    Modules in python

--- the_basics/basics.py
+++ the_basics/basics.py
@@ -1,14 +1,57 @@
 import datetime
+
+"""
 print("The Date and Time is",datetime.datetime.now())
 from os import path
 
-file_path = path.relpath("AddedFiles/fruit.txt")
+file_path = path.relpath("files/fruit.txt")
 
 myfile = open(file_path,"r")
 content = myfile.read()
 myfile.close()
 
+with open(file_path,"w") as myfile:
+    myfile.write("Tomato n Cucumber n Onion")
+
 with open(file_path,"r") as myfile1:
     content = myfile1.read()
 
 print(content)    
+
+with open("files/fruits.txt","x") as myfile2: 
+    myfile2.write("Okra") 
+
+"""
+
+'''  Text Appending to a File
+with open("files/data.txt","a+") as data:
+    data.seek(0)
+    ctnt = data.read()
+    data.seek(0) 
+    data.write("\n" + ctnt)
+    data.write("\n" + ctnt)
+'''
+"""
+System Modeules
+import sys
+sys.builtin_module_names
+
+
+import time
+import os
+while True:
+    if os.path.exists("files/vegetables.txt"):
+        with open("files/vegetables.txt") as vegs:
+            print(vegs.read())
+    else:
+        print("File does not exist.")        
+    time.sleep(10)
+"""
+
+"""
+Third - Party Modules
+
+
+
+
+"""
