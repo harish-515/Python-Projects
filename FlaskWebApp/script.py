5fcca7bbf950af391e5336bdccee5d0407023c32
Harish Bugata <harish.bugata@hotmail.com>
2019-10-02 22:12:57 +0530

    render templates in flask

--- FlaskWebApp/script.py
+++ FlaskWebApp/script.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
+"""
+We use render template method of flask to create html content
+To use the render template we need to have the html files in a "templates" folder
+in the same folder as script.py
 
+"""
 #flask app variable
 app=Flask(__name__)
+
 
 #Routes
 @app.route('/')
 def home():
-    return "Homepage here!"
+    return render_template("home.html")
 
 @app.route('/about/')
 def about():
-    return "About content goes here!"
+    return render_template("about.html")
 
 """
 when we run the script.py the __name__ = "__main__"
